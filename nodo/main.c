203bf4596cc8f1492d8dcde925d3f45e8f12c215
Enrique Hernandez <piedraquique@gmail.com>
2026-03-09 23:42:04 -0600

    Main.C completado!!

--- nodo/main.c
+++ nodo/main.c
@@ -5,26 +5,15 @@ int main()
 {
     printf("Prueba del TAD Nodo\n");
 
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
+    Nodo* n = crearNodo(10);
     Nodo* n = NULL;
 
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
+if (n != NULL) {
+        printf("Valor inicial: %d\n", obtenerValor(n));
+    }
+asignarValor(n, 25);
 
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
-
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
-
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
-
+printf("Nuevo valor: %d\n", obtenerValor(n)); 
+destruirNodo(n);
     return 0;
 }
